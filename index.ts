a8f5bedec44a41b97bfb08bb62bb3417193e3405
Aleeze <162796538+Aleeze123@users.noreply.github.com>
2024-06-23 22:26:03 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,43 @@
+import inquirer from "inquirer";
+import chalk from "chalk";
+
+console.log(chalk.underline.bold.yellow.yellow("\n\tWELCOME TO ALEEZE STUDENT GRADE CHECKER USING SWITCH CASE CONDITION...!"));
+
+interface studentName {
+    name: string;
+}
+
+interface studentMarks {
+    marks: string; 
+}
+let student: studentName = await inquirer.prompt({
+    name: "name",
+    message: "Enter student name....."
+});
+
+let marksInput: studentMarks = await inquirer.prompt({
+    name: "marks",
+    message: "Enter student marks....."
+});
+
+let marks: number = parseInt(marksInput.marks); // Convert the input to an integer
+
+switch (true) {
+    case (marks > 80 && marks <= 100):
+        console.log(chalk.underline.cyan(`${student.name} has secured an A grade.....`));
+        break;
+    case (marks > 60 && marks <= 80):
+        console.log(chalk.underline.green(`${student.name} has secured a B grade.....`));
+        break;
+    case (marks > 40 && marks <= 60):
+        console.log(chalk.underline.magenta(`${student.name} has secured a C grade.....`));
+        break;
+    case (marks >= 0 && marks <= 40):
+        console.log(chalk.underline.red(`${student.name} has secured a D grade.....`));
+        break;
+    default:
+        console.log(chalk.underline.red(`Invalid marks entered.....`));
+        break;
+}
+console.log("-".repeat(50));
+
